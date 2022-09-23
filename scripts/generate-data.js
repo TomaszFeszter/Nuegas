44560d0bc8dbd1c1b6d4df359b676fbf3b80ce86
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-09-23 16:42:41 +0200

    new functions, updated correct data

--- scripts/generate-data.js
+++ scripts/generate-data.js
@@ -1,5 +1,10 @@
 import { faker } from "@faker-js/faker";
-import { range, random } from "../utils/index.js";
+import {
+  range,
+  random,
+  getRandomUniqueArrValues,
+  randomIntNoZero,
+} from "../utils/index.js";
 import fs from "fs";
 
 const usersArr = range(20, () => ({
@@ -14,40 +19,48 @@ const usersArr = range(20, () => ({
 const teamsArr = range(5, () => ({
   id: faker.datatype.uuid(),
   name: faker.company.name(),
-  members: range(random(6), () => usersArr[random(19)].id),
+  members: getRandomUniqueArrValues(randomIntNoZero(8), usersArr),
 }));
 
 const tasksArr = range(15, () => ({
   id: faker.datatype.uuid(),
-  title: faker.lorem.sentence(random(10)),
+  title: faker.lorem.sentence(randomIntNoZero(10)),
   description: faker.lorem.lines(2),
   createdBy: usersArr[random(19)],
   deadline: faker.date.soon(30),
-  objectives: range(random(8), () => ({
-    title: faker.lorem.sentence(random(8)),
+  objectives: range(randomIntNoZero(8), () => ({
+    title: faker.lorem.sentence(randomIntNoZero(8)),
     complete: faker.datatype.boolean(),
   })),
   isActive: faker.datatype.boolean(),
 }));
 
-const boardArr = range(4, () => ({
+const comentsArr = range(20, () => ({
+  taskId: tasksArr[random(14)].id,
+  userId: usersArr[random(19)].id,
+  content: faker.lorem.lines(randomIntNoZero(3)),
+}));
+
+const boardArr = range(5, () => ({
   id: faker.datatype.uuid(),
-  title: faker.lorem.sentence(random(5)),
+  title: faker.lorem.sentence(randomIntNoZero(5)),
   createdBy: usersArr[random(19)],
-  columns: {
-    todo: range(random(10), () => tasksArr[random(14)]),
-    inProgress: range(random(5), () => tasksArr[random(14)]),
-    readyForCodeRev: range(random(5), () => tasksArr[random(14)]),
-    inCodeRev: range(random(5), () => tasksArr[random(14)]),
-    done: range(random(3), () => tasksArr[random(14)]),
-  },
+}));
+
+const boardColumnsArr = range(5, () => ({
+  id: faker.datatype.uuid(),
+  boardId: boardArr[random(4)],
+  title: faker.lorem.sentence(randomIntNoZero(5)),
+  tasks: getRandomUniqueArrValues(randomIntNoZero(10), tasksArr),
 }));
 
 const dataObj = {
   users: usersArr,
   teams: teamsArr,
   tasks: tasksArr,
+  comments: comentsArr,
   boards: boardArr,
+  boardColumns: boardColumnsArr,
 };
 
 fs.writeFileSync("./mock/db.json", JSON.stringify(dataObj));
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,8 @@
 export const random = (max = 10) => Math.trunc(Math.random() * max);
 
+export const randomIntNoZero = (max = 10) =>
+  Math.trunc(Math.random() * (max - 1) + 1);
+
 export const range = (num = 0, func = () => {}) => {
   const arr = [];
 
@@ -8,4 +11,22 @@ export const range = (num = 0, func = () => {}) => {
   }
 
   return arr;
+};
+
+export const uniqueRandomIndexNumbers = (quantity, max) => {
+  const randomIndexNumbers = [];
+  while (randomIndexNumbers.length < quantity) {
+    const randomNum = random(max);
+    if (randomIndexNumbers.indexOf(randomNum) === -1)
+      randomIndexNumbers.push(randomNum);
+  }
+  return randomIndexNumbers;
+};
+
+export const getRandomUniqueArrValues = (quantity, arr) => {
+  const randomValues = [];
+  uniqueRandomIndexNumbers(quantity, arr.legth).map((index) =>
+    randomValues.push(arr[index])
+  );
+  return randomValues;
 };
