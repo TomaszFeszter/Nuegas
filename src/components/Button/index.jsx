144e12a24969d0c506f4d33b6f19b0b0e9c1996f
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-10-17 15:46:36 +0200

    add colors props

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: ${(props) => props.theme.textWhite};
+  background: ${(props) => props.theme.blue700};
+  border: none;
+`;
---
+++ src/components/themes/index.jsx
@@ -0,0 +1,29 @@
+export const lightTheme = {
+  textWhite: "#FFFFFF",
+  textDark800: "#141522",
+  textDark500: "#54577A",
+  textDark300: "#8E92BC",
+  dark800: "#141522",
+  dark500: "#54577A",
+  light700: "#EBEDF7",
+  light500: "#F5F5F7",
+  light200: "#FAFAFA",
+  blue700: "#546FFF",
+  blue300: "#BAC8FF",
+  red500: "#FF4D5E",
+};
+
+export const darkTheme = {
+  textWhite: "#FFFFFF",
+  textDark800: "#141522",
+  textDark500: "#54577A",
+  textDark300: "#8E92BC",
+  dark800: "#141522",
+  dark500: "#54577A",
+  light700: "#EBEDF7",
+  light500: "#F5F5F7",
+  light200: "#FAFAFA",
+  blue700: "#546FFF",
+  blue300: "#BAC8FF",
+  red500: "#FF4D5E",
+};
