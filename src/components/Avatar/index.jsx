7cf3f415cbb5ed4ccc05fdd0866ad7f19e592854
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-10-24 09:37:20 +0200

    small changes in Avatar component, add dark mode to storybook

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -12,19 +12,18 @@ const AvatarComponent = ({ className, src }) => {
 export const Avatar = styled(AvatarComponent)`
   img {
     border-radius: 50%;
+    ${(props) => props.border && `border: 1.5px solid ${props.theme.white}; `};
     ${(props) =>
       props.border
-        ? `width: 4.9rem; height: 4.9rem; border: 1.5px solid ${props.theme.white}; `
+        ? `width: 4.9rem; height: 4.9rem; `
         : "width: 5.2rem; height: 5.2rem;"};
     ${(props) =>
       props.border
-        ? props.medium &&
-          `width: 4.5rem; height: 4.5rem; border: 1.5px solid ${props.theme.white}; `
+        ? props.medium && `width: 4.5rem; height: 4.5rem; `
         : props.medium && "width: 4.8rem; height: 4.8rem;"};
     ${(props) =>
       props.border
-        ? props.small &&
-          `width: 2.1rem; height: 2.1rem; border: 1.5px solid ${props.theme.white}; `
+        ? props.small && `width: 2.1rem; height: 2.1rem; `
         : props.small && "width: 2.4rem; height: 2.4rem;"};
   }
 `;
